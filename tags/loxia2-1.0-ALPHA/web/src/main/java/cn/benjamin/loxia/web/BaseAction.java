e0566ee3ffecf3db633e5920c336ebd28a2cdfeb
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2009-12-29 08:27:54 +0000

    first alpha release for loxia2

---
+++ tags/loxia2-1.0-ALPHA/web/src/main/java/cn/benjamin/loxia/web/BaseAction.java
@@ -0,0 +1,95 @@
+package cn.benjamin.loxia.web;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.RequestAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.LocalizedTextUtil;
+
+public class BaseAction extends ActionSupport implements RequestAware{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4113598733962053745L;
+	
+	protected static final Logger logger = LoggerFactory.getLogger(BaseAction.class);
+	
+	public static final String FOLLOWING_URL_AFTER_OPERATING_UNIT_PICKUP = "BaseAction.followingUrl";
+
+	public static final String JSON = "json";
+	
+	private String acl;
+	private Long selectedOuId;	
+	
+	protected int pageSize;
+	protected int currentPage;
+	protected String sortString;	
+	
+	@SuppressWarnings("unchecked")
+	protected Map request;
+
+	@SuppressWarnings("unchecked")
+	public void setRequest(Map request) {
+		this.request = request;
+	}
+
+	protected String getMessage(String key, Object[] args){
+		Locale locale = ActionContext.getContext().getLocale();
+		return LocalizedTextUtil.findText(this.getClass(), key, locale, key, args);
+	}
+	
+	public void addActionError(String errKey, Object[] args){
+		addActionError(getMessage(errKey, args));
+	}
+
+	public void addFieldError(String fieldName, String errKey, Object[] args){
+		addFieldError(fieldName, getMessage(errKey, args));
+	}
+
+	public void addActionMessage(String msgKey, Object[] args){
+		addActionMessage(getMessage(msgKey, args));
+	}
+	
+	public String getAcl() {
+		return acl;
+	}
+	public void setAcl(String acl) {
+		this.acl = acl;
+	}
+	public Long getSelectedOuId() {
+		return selectedOuId;
+	}
+	public void setSelectedOuId(Long selectedOuId) {
+		this.selectedOuId = selectedOuId;
+	}
+	
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public String getSortString() {
+		return sortString;
+	}
+
+	public void setSortString(String sortString) {
+		this.sortString = sortString;
+	}
+}
