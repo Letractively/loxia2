f2a2a8e2df4ad19592ef872f418888df63753c07
Alex.Wei1988 <Alex.Wei1988@ca9f5868-c45d-11de-9233-4169c94807ec>
2010-01-15 17:16:03 +0000

    add new function of project =>add ,modify user.

--- samples/basic-webapp/src/main/java/cn/benjamin/loxia/examples/action/UserMaintainAction.java
+++ samples/basic-webapp/src/main/java/cn/benjamin/loxia/examples/action/UserMaintainAction.java
@@ -1,5 +1,7 @@
 package cn.benjamin.loxia.examples.action;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -7,13 +9,21 @@ import java.util.List;
 import java.util.Map;
 
 import cn.benjamin.loxia.examples.dao.UserDao;
+import cn.benjamin.loxia.examples.dao.UserInformationDao;
 import cn.benjamin.loxia.examples.dao.UserMemoDao;
+import cn.benjamin.loxia.examples.manager.UserInformationManager;
 import cn.benjamin.loxia.examples.manager.UserManager;
 import cn.benjamin.loxia.examples.manager.UserMemoManager;
+import cn.benjamin.loxia.examples.model.UserInformation;
 import cn.benjamin.loxia.examples.model.UserMemo;
+import cn.benjamin.loxia.model.OperatingUnit;
 import cn.benjamin.loxia.model.User;
+import cn.benjamin.loxia.support.image.ImageResizer;
 import cn.benjamin.loxia.support.json.JSONObject;
 import cn.benjamin.loxia.utils.DateUtil;
+import cn.benjamin.loxia.utils.FileUtil;
+import cn.benjamin.loxia.utils.PropListCopyable;
+import cn.benjamin.loxia.utils.PropertyUtil;
 import cn.benjamin.loxia.web.BaseProfileAction;
 import cn.benjamin.loxia.web.annotation.Acl;
 import cn.benjamin.loxia.web.annotation.DataResponse;
@@ -26,18 +36,28 @@ public class UserMaintainAction extends BaseProfileAction {
 	private static final long serialVersionUID = 2519287147927865666L;
 	
 	private User user;
+	private UserInformation userInformation;
+	private String portraitContentType;
+	private String portraitFileName;
+	private File portrait;
 	//TODO add date selection
 	private Date date;		
 
 	private List<UserMemo> userMemos = new ArrayList<UserMemo>();	
-
+	
+	private final OperatingUnit ou=new OperatingUnit();
+	
 	private UserDao userDao;
 	private UserManager userManager;
 	private UserMemoDao userMemoDao;	
 	private UserMemoManager userMemoManager;	
+	private UserInformationDao userInformationDao;	
+	private UserInformationManager userInformationManager;
+	
 
 	@SuppressWarnings("unchecked")
 	private void prepareForTodoListMaintain(){
+		
 		request.put("umList", 
 				userMemoDao.findUserMemosByUserIdAndDate(userDetails.getUser().getId(), 
 						date == null ? DateUtil.today() : DateUtil.roundDate(date)));
@@ -58,17 +78,72 @@ public class UserMaintainAction extends BaseProfileAction {
 		return SUCCESS;
 	}
 	
+	@Acl("ACL_USER_MAINTAIN")
 	@SuppressWarnings("unchecked")
 	@DataResponse
 	public String addUser() throws Exception{		
 		user.setId(null);
+		userInformation.setId(null);
+		ou.setId(1l);
+		user.setOu(ou);
+		userInformation.setUser(user);
 		user = userDao.save(user);
+		userInformationManager.saveOrUpdate(userInformation);
 		Map<String,Object> result = new HashMap<String, Object>();
 		result.put("result", true);
 		request.put("json", new JSONObject(result));
 		return JSON;
 	}
-
+	
+	//just load user and userInformation and redirect
+	@Acl("ACL_USER_MAINTAIN")
+	@SuppressWarnings("unchecked")
+	public String editUser()
+	{
+		user=userDao.getByPrimaryKey(user.getId());
+		userInformation=userInformationDao.findUserInformationByUser(user.getId());
+		
+		return JSON;
+	}
+	//save the modified user and ui
+	public String modifyUser()
+	{
+		    try {
+		    	userManager.EditUser(user, userInformation);
+		    	Map<String,Object> result = new HashMap<String, Object>();
+				result.put("result", true);
+				request.put("json", new JSONObject(result));
+				
+			} catch (Exception e) {
+				if(logger.isDebugEnabled())
+					e.printStackTrace();
+				request.put("errorMessage", "System error.");
+			}
+		return JSON;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String uploadPortrait(){
+		try{
+			logger.debug("upload portrait's content type: {}", portraitContentType);
+			if(portraitContentType.indexOf("image") ==0){					
+				byte[] portraits = ImageResizer.resizeImageAsJPG(FileUtil.getBytesFromFile(portrait), 72, 92); 
+				UserInformation infor =
+					userInformationManager.updatePortrait(user.getId(), portraits);
+				UserInformation ui = new UserInformation();
+				PropertyUtil.copyProperties(infor, ui, new PropListCopyable("id","habbit","description"));
+				request.put("userInformation", infor);
+			}else{
+				request.put("errorMessage", "Invalid picture.");
+			}
+		}catch(Exception e){
+			if(logger.isDebugEnabled())
+				e.printStackTrace();
+			request.put("errorMessage", "Portrait upload failure.");
+		}
+		return SUCCESS;
+	}
+	
 	@SuppressWarnings("unchecked")
 	@DataResponse
 	public String checkUserUnique() throws Exception{
@@ -83,6 +158,40 @@ public class UserMaintainAction extends BaseProfileAction {
 		return JSON;
 	}
 	
+	public String getPortraitContentType() {
+		return portraitContentType;
+	}
+
+	public void setPortraitContentType(String portraitContentType) {
+		this.portraitContentType = portraitContentType;
+	}
+
+	public String getPortraitFileName() {
+		return portraitFileName;
+	}
+
+	public void setPortraitFileName(String portraitFileName) {
+		this.portraitFileName = portraitFileName;
+	}
+
+	public File getPortrait() {
+		return portrait;
+	}
+
+	public void setPortrait(File portrait) {
+		this.portrait = portrait;
+	}
+
+	public UserInformationManager getUserInformationManager() {
+		return userInformationManager;
+	}
+
+	public void setUserInformationManager(
+			UserInformationManager userInformationManager) {
+		this.userInformationManager = userInformationManager;
+	}
+
+
 	@Acl(value="ACL_USERMEMO_MAINTAIN")
 	@SuppressWarnings("unchecked")
 	@DataResponse
@@ -137,6 +246,24 @@ public class UserMaintainAction extends BaseProfileAction {
 	public void setUserMemoManager(UserMemoManager userMemoManager) {
 		this.userMemoManager = userMemoManager;
 	}
+
+	
+	public UserInformation getUserInformation() {
+		return userInformation;
+	}
+
+	public void setUserInformation(UserInformation userInformation) {
+		this.userInformation = userInformation;
+	}
+
+	public UserInformationDao getUserInformationDao() {
+		return userInformationDao;
+	}
+
+	public void setUserInformationDao(UserInformationDao userInformationDao) {
+		this.userInformationDao = userInformationDao;
+	}
+	
 	
 	public UserManager getUserManager() {
 		return userManager;
@@ -145,4 +272,5 @@ public class UserMaintainAction extends BaseProfileAction {
 	public void setUserManager(UserManager userManager) {
 		this.userManager = userManager;
 	}
+
 }
