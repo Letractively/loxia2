2697bcee6bc84dc3705050f65d8a663c27287360
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2010-03-17 09:49:25 +0000

    add spring mvc support

--- samples/rest/src/main/java/cn/benjamin/loxia/examples/controller/RootController.java
+++ samples/rest/src/main/java/cn/benjamin/loxia/examples/controller/RootController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.benjamin.loxia.springmvc.BaseProfileController;
+import cn.benjamin.loxia.web.annotation.Acl;
 
 @Controller
 public class RootController extends BaseProfileController{
 
 	@RequestMapping("/desktop")
+	@Acl("ACL_DO_NOT_HAVE")
 	public String desktop(Model model){
+		System.out.println("User:" + getCurrentUser().getLoginName());
+		System.out.println("Current Ou:" + getCurrentOperatingUnit());
 		return "desktop";
 	}
 }
